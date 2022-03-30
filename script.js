63be3162875bb92f6289426ee0ee76ed1281ba3f
xDmNkx <dominik.nikiel@o2.pl>
2022-03-30 20:30:57 +0200

    Aktualizacja z JS

---
+++ script.js
@@ -0,0 +1,16 @@
+//STAŁA (NIE MOŻNA JEJ NADPISAĆ)
+const
+//ZMIENNA (MOŻE SIĘ ZMIENIAĆ)
+let
+//STRING (CIAG ZNAKOW) - ZAWSZE W CUDYSŁOWIU
+const string = "przykład"
+//OBJEKTY
+const project = {
+    name : "Projekt 1",
+    data : "30.03.2022"
+}
+//console.log("Projekt o nazwie" + project.name
+//+ "został stworzony" + project.date)
+console.log(console)
+
+
